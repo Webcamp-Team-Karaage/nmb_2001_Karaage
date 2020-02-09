64bebbb05e7be2ba95d04ac438ab290a7da9fbe7
NodokaAoki <genta91nya@gmail.com>
2020-02-09 20:40:03 +0000

    [Add]homes

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -52,7 +52,7 @@ class Members::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
-  #   super(resource)
+  # redirect_to root_path
   # end
 
   # The path used after sign up for inactive accounts.
