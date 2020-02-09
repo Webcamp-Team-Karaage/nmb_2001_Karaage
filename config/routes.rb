739450d9a09cd268d69345171c9eda391bf675e9
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-02-09 08:43:26 +0000

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#top"
 
-  resources :home, only: [:top, :about]
+  get 'home/top' => 'homes#top'
+  get 'home/about' => 'homes#about'
+  get 'members/:id/delete_me' => 'members#delete_me'
+  delete 'cart_products' => 'cart_products#destroy_all'
+  get 'members/:id/orders/input' => 'orders#input'
+  get 'orders/thanks' => 'orders#thanks'
+  get 'admins/top' => 'admins#top'
 
-  resources :members_registrations, only: [:new, :create]
-  resources :members_sessions, only: [:new, :create, :destroy]
-  resources :members, only: [:show, :delete_me, :edit, :update]
   resources :shipping_addresses, only: [:index, :create, :destroy, :edit, :update]
-  resources :cart_products, only: [:index, :create, :update, :destroy, :destroy_all]
-  resources :orders, only: [:index, :show, :input, :new, :create, :thanks]
+  resources :cart_products, only: [:index, :create, :update, :destroy]
+  resources :orders, only: [:index, :show, :new, :create]
   resources :products, only: [:index, :show]
 
-  resources :admins, only: [:top]
-  resources :admins_sessions, only: [:new, :create, :destroy]
-  resources :admin_members, only: [:index, :show, :edit, :update]
   resources :admin_orders, only: [:index, :show, :update]
   resources :admin_order_products, only: [:update]
   resources :admin_products, only: [:index, :new, :create, :show, :edit, :update]
