6c053e7699331dec0ebaa8eec804ad70ab61d045
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-02-09 07:33:40 +0000

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#top"
+
+  resources :home, only: [:top, :about]
+
+  resources :members_registrations, only: [:new, :create]
+  resources :members_sessions, only: [:new, :create, :destroy]
+  resources :members, only: [:show, :delete_me, :edit, :update]
+  resources :shipping_addresses, only: [:index, :create, :destroy, :edit, :update]
+  resources :cart_products, only: [:index, :create, :update, :destroy, :destroy_all]
+  resources :orders, only: [:index, :show, :input, :new, :create, :thanks]
+  resources :products, only: [:index, :show]
+
+  resources :admins, only: [:top]
+  resources :admins_sessions, only: [:new, :create, :destroy]
+  resources :admin_members, only: [:index, :show, :edit, :update]
+  resources :admin_orders, only: [:index, :show, :update]
+  resources :admin_order_products, only: [:update]
+  resources :admin_products, only: [:index, :new, :create, :show, :edit, :update]
+  resources :admin_genres, only: [:index, :create, :edit, :update]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_02_09_050356) do
 
-ActiveRecord::Schema.define(version: 2020_02_08_044430) do
+  create_table "cart_products", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "product_id"
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +30,31 @@ ActiveRecord::Schema.define(version: 2020_02_08_044430) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "payment"
+    t.string "address_name"
+    t.string "postal_code"
+    t.text "address"
+    t.integer "postage"
+    t.integer "status"
+    t.string "total_price"
+    t.string "float"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "text"
+    t.integer "price"
+    t.integer "status"
+    t.string "product_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
