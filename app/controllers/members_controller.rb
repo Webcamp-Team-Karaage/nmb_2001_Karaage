bc7603e591f94920f4a3cdbc26e8d558e0d0626a
NodokaAoki <genta91nya@gmail.com>
2020-02-09 18:08:29 +0000

    一旦

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,8 +4,22 @@ class MembersController < ApplicationController
   end
 
   def edit
+  	@member = Member.find(params[:id])
   end
 
   def update
+  	@member = Member.find(params[:id])
+  	@member.update(member_params)
+  	redirect_to member_path(@member.id)
   end
+
+  def delete_me
+  	@member = Member.find(params[:id])
+  end
+
+  private
+  def member_params
+  	params.require(:member).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone)
+  end
+
 end
