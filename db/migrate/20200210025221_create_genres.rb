1c7b7efca3458f7abe48a0a265b7c017f6a3fbcf
yasuhara000 <kaaazumakun123@gmail.com>
2020-02-10 06:16:48 +0000

    file chenge

--- db/migrate/20200210025221_create_genres.rb
+++ db/migrate/20200210025221_create_genres.rb
@@ -1,7 +1,7 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
-      t.integer :name
+      t.string :name
       t.boolean :status
       t.datetime :created_at
       t.datetime :updated_at
