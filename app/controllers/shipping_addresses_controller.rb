1d2c906311ed9e9c0dbcd7c0990973765690c75e
yusuke-matsukawa <yuumatsu8970@icloud.com>
2020-02-10 10:38:15 +0000

    [Add]sippingaddress

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,2 +1,22 @@
 class ShippingAddressesController < ApplicationController
+
+	def index
+		@sipping_addresses = Sippin_gaddress.all
+		@sipping_address = Sipping_address.new
+	end
+
+	def create
+		@sipping_address = Book.new(sipping_address_params)
+		if @bsipping_address.save
+			redirect_to sipping_address_path(@sipping_address)
+		else
+			@sipping_addresses = Sippin_gaddress.all
+			render :index
+		end
+	end
+
+	private
+	def sipping_address_params
+		params.require(:sipping_address).permit(:postal_code, :address, :address_name)
+	end
 end
