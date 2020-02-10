f6cf16f2eafe0046ca381ad7d206e321b6e40759
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-02-10 09:44:32 +0000

    add

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :cart_products, dependent: :destroy
 	has_many :order_products, dependent: :destroy
-	belongs_to :genre	
+	belongs_to :genre
+	attachment :image #画像アップ用ﾒｿｯﾄﾞ
 end
