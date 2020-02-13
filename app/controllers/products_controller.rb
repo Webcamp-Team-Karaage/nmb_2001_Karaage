0cc60fce89360426e8d65854b9203eac3eab7404
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-02-13 04:45:28 +0000

    fix many view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,6 @@ class ProductsController < ApplicationController
 		@products = Product.all #商品全件載せる
 	end
 	def show
-		@product = Product.find(product_params[:id])
-	end
-
-	private
-	def product_params
-		params.require(:product).permit(:count)
+		@cart_product = CartProduct.new
 	end
 end
