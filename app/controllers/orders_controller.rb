41bb58e222df0676e6531ed762dea31f95ac42dc
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2020-02-14 08:46:44 +0000

    add route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
 		@shipping_addresses = ShippingAddress.all
 	end
 	def new
-		@order = Order.new
+		@order = Order.new(order_params)
 		@orders = Order.all
 		#@order = Order.find(order_params[:id])
 		#order.save
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
 
 	private
 	def order_params
-		params.require(:order).permit(:postal_code, :address, :address_name)
+		params.require(:order).permit(:payment, :postal_code, :address, :address_name)
 	end
 	def shipping_address_params
 		params.require(:shipping_address).permit(:postal_code, :address, :address_name)
