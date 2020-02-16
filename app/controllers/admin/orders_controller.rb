8a662011661bd3e69f1985895055b3c65b5b4aca
yusuke-matsukawa <yuumatsu8970@icloud.com>
2020-02-16 10:23:01 +0000

    一旦

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,17 @@
 class Admin::OrdersController < ApplicationController
 	def index
-		@admin_orders = Order.all
+	   @admin_orders = Order.all
 
-		# @order = @admin_orders.order_product
+       # @total_count = []
+       # @admin_orders.each do |order|
+       #   sum = 0
+       #   order.order_products.each do |order_product|
+       #      sum += order_product.count
+       #   end
+       #   @total_count << sum
+       # end
+       # puts @total_count
 
-		# @order.each do |order|
-     		# @orderproduct.each do |orderproduct|
-        		# count1 = order.orderproduct.count
-            	# count = count + count1
-     		# end
-		# end
 	end
 
 	def show
