ac7062f473260500f5d9440130f4ddea341b4189
yusuke-matsukawa <yuumatsu8970@icloud.com>
2020-02-17 07:43:09 +0000

    一旦

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,18 +2,25 @@ class Admin::OrdersController < ApplicationController
 	def index
 	   @admin_orders = Order.all
 
-       # @total_count = []
-       # @admin_orders.each do |order|
-       #   sum = 0
-       #   order.order_products.each do |order_product|
-       #      sum += order_product.count
-       #   end
-       #   @total_count << sum
-       # end
-       # puts @total_count
+       @total_count = {}
+       @admin_orders.each do |order|
+         sum = 0
+         order.order_products.each do |order_product|
+            sum += order_product.count
+         end
+         @total_count[order.id] = sum
+       end
+       puts @total_count
 
 	end
 
 	def show
+		@admin_order = Order.find(params[:id])
+		@orders = Order.where(status: 0)
+	end
+
+	private
+	def order_params
+  	params.require(:order).permit(:payment, :address_name, :postal_code, :address, :postage, :status, :total_price, :float)
 	end
 end
