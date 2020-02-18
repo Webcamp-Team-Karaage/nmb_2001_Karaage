82338f8cafdf6ccda82e081d888ae2ed4eec9db5
yasuhara000 <kaaazumakun123@gmail.com>
2020-02-18 08:00:42 +0000

    first

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -18,17 +18,17 @@ class Admin::OrdersController < ApplicationController
                 @title = "全"
         end
 
-=begin
+
         @total_count = {}
         @admin_orders.each do |order|
             sum = 0
-         order.order_products.each do |order_product|
-             sum += order_product.count
-         end
-         @total_count[order.id] = sum
+         	order.order_products.each do |order_product|
+            	sum += order_product.count
+         	end
+         	@total_count[order.id] = sum
         end
-        puts @total_count
-=end
+        
+		
 	end
 
 	def show
