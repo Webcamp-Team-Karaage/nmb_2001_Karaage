248aedd71a9e3a0409c4d25784e8e229a92f6a34
yusuke-matsukawa <58587632+yusuke-matsukawa@users.noreply.github.com>
2020-02-18 17:04:07 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,7 @@ class Order < ApplicationRecord
 
 	enum payment:{クレジットカード: 0, 銀行振込: 1}
 	enum status:{入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
-<<<<<<< HEAD
-	
-=======
 
 	validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
->>>>>>> deb9201f7552af9f67d8b8de5e793ccc52ed9e45
+
 end
