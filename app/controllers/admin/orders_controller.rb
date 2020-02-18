3de3624c83ee600620f371a66b5e6833c9ee60d9
yasuhara000 <kaaazumakun123@gmail.com>
2020-02-18 07:51:46 +0000

    first

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,7 +16,7 @@ class Admin::OrdersController < ApplicationController
 
 	def show
 		@admin_order = Order.find(params[:id])
-
+		@order_product = OrderProduct.where(order_id:@admin_order.id)
 		@order_products = @admin_order.order_products
 		@price_sum = 0
 		@admin_order.order_products.each do |order_product|
@@ -27,13 +27,81 @@ class Admin::OrdersController < ApplicationController
 	end
 
 	def update
-		@admin_order = Order.find(params[:id])
-		@admin_order.save
+		@admin_order = Order.find(params[:id]) 
+		@order_products = OrderProduct.where(order_id: params[:id])
+		#@order_product = @order_products.find_by(id: params[:order_product][:product_id])
+
+		if params[:order_product]
+			@order_products.each do |order_product|
+				if params[:order_product][:status] == "製作中"
+					@admin_order.status = "製作中"
+					@admin_order.update(status: @admin_order.status)
+					order_product.update(order_product_params)
+				elsif params[:order_product][:status] == "製作完了"
+					@admin_order.status = "発送準備中"
+					@admin_order.update(status: @admin_order.status)
+					order_product.status = params[:order_product][:status]
+					order_product.update(order_product_params)
+				else
+					order_product.update(order_product_params)
+				end
+			end
+		elsif params[:order]
+			
+			#@order_product = @order_products.find_by(id: params[:order][:product_id])
+			if params[:order][:status] == "入金確認"
+				@admin_order.update(order_params)
+				@order_products.each do |order_product|
+					order_product.status = "製作中"
+					order_product.update(status: order_product.status)
+				end			
+			else
+				@admin_order.update(order_params)
+			end
+			
+		end
+
+		
+		
+		#order_product
+=begin
+		if params[:order_product][:status] == "製作中"
+			@admin_order.status = "制作中"
+			@admin_order.update(status: @admin_order.status)
+			@order_product.update(order_product_params)
+		elsif params[:order_product][:status] == "製作完了"
+			@admin_order.status = "発送準備中"
+			@admin_order.update(status: @admin_order.status)
+			@order_product.status = params[:order_product][:status]
+			@order_product.update(order_product_params)
+		else
+			@order_product.update(order_product_params)
+		end
+
+		#order
+		if params[:order][:status] == "入金確認"
+			@order_product.status = "制作中"
+			@order_product.update(status: @order_product.status)
+		else
+			@order.update(order_params)
+		end
+=end
+
+
+		
+		
+		
+		
 		redirect_to admin_order_path
 	end
 
 	private
 	def order_params
-  	params.require(:order).permit(:payment, :address_name, :postal_code, :address, :postage, :status, :total_price, :float)
+		
+			params.require(:order).permit(:payment, :address_name, :postal_code, :address, :postage, :status, :total_price, :float,:product_id)
 	end
+	def order_product_params
+		params.require(:order_product).permit(:status)
+	end
+		
 end
